25bfcb1c9c4a6fd3cbf035e9e609001a9bdfaab3
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-07-13 11:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+
+
+let str = prompt("Enter a String: ");
+
+  for (let i = 1; i < str.length; i++) {
+  	let ct = 1;
+	  for (let j = i+1; j < str.length; j++) {
+	  	    if (str.charAt(i) == str.charAt(i-1)) {
+	  	    	ct++;
+	  	    }
+	  }
+
+	  if (ct == 1) {
+	  	alert(str.charAt(i));
+		break;
+	  }
+  }
